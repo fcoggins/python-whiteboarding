4d69f78b1c32a6728ff9766b3ee5c22b490d6866
Florie Coggins <florie.coggins@gmail.com>
2014-12-21 14:30:00 -0500

    Every other chair in a circle removed using linked lists

---
+++ musical_chairs.py
@@ -0,0 +1,25 @@
+import linkedlists
+
+def find_last(n):
+    '''You have a circle of n chairs. Remove every other chair continuously until 
+    there is one chair left. What is the last remaining chair. Solution uses linked lists.
+
+    >>> find_last(5)
+    3
+    '''
+
+    cycle = linkedlists.Cycle(n)
+    node = cycle.head
+    prev = cycle.head
+    counter = 0
+    while node != node.next:
+        if counter%2 != 0:
+            prev.next = node.next
+            prev = node.next
+        node = node.next
+        counter += 1
+    return node.next.data + 1
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
